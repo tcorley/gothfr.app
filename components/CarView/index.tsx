dda24d944f484abc19a3510102069fa2da9f848b
Tyler Corley <tyler_corley@me.com>
2020-12-05 10:17:06 -0800

    Adding nice heading to car view with circle text

--- components/CarView/index.tsx
+++ components/CarView/index.tsx
@@ -1,5 +1,7 @@
-import useGoogleSheets from 'use-google-sheets';
-import styles from '../../styles/CarView.module.css';
+import { useEffect } from "react";
+import useGoogleSheets from "use-google-sheets";
+import CircleType from "circletype";
+import styles from "../../styles/CarView.module.css";
 
 type Totals = {
   paid: string;
@@ -14,14 +16,21 @@ type Payment = {
 
 type Props = {
   onLogout: () => void;
-}
+};
 
 export default function CarView({ onLogout }: Props) {
   const { data, loading, error } = useGoogleSheets({
     apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
     sheetId: process.env.NEXT_PUBLIC_GOOGLE_SHEETS_ID,
-    sheetsNames: ['payments', 'totals'],
+    sheetsNames: ["payments", "totals"],
   });
+
+  useEffect(() => {
+    const heading = new CircleType(document.getElementById("heading"));
+    const updateRadius = () => heading.radius(heading.element.offsetWidth / 2);
+    window.addEventListener("resize", updateRadius);
+    updateRadius();
+  }, []);
 
   let paidAmount;
   let lastPayment;
@@ -38,6 +47,7 @@ export default function CarView({ onLogout }: Props) {
 
   return (
     <div>
+      <h1 id="heading">HANK YOU VERY MUCH!</h1>
       <img
         src="/xbfront.webp"
         width="500"
@@ -48,32 +58,25 @@ export default function CarView({ onLogout }: Props) {
         {loading && <p>loading</p>}
         {data.length > 0 && (
           <p>
-            So far you've paid{' '}
-            <span className={styles.emphasis}>${paidAmount}</span> of{' '}
+            So far you've paid{" "}
+            <span className={styles.emphasis}>${paidAmount}</span> of{" "}
             <span className={styles.emphasis}>${initialAmount}</span>, which
             leaves <span className={styles.emphasis}>${remainingAmount}</span>.
-            Your last payment was{' '}
-            <span className={styles.emphasis}>${lastPayment.amount}</span> on{' '}
+            Your last payment was{" "}
+            <span className={styles.emphasis}>${lastPayment.amount}</span> on{" "}
             <span className={styles.emphasis}>{lastPayment.date}</span>.
           </p>
         )}
         <div className={styles.flex}>
-        <a href={'sms:' + process.env.NEXT_PUBLIC_PHONE_NUMBER} >
-          <img
-          src="/moneypayment.gif"
-          width="150"
-          />
-        </a>
-        <span className={styles.divider} />
-        <div className={styles.flex} onClick={onLogout}>
-        <img
-         src="/bomb.gif"
-         width="100"
-         height="100"
-         />
-         <p>logout</p>
-         </div>
-         </div>
+          <a href={"sms:" + process.env.NEXT_PUBLIC_PHONE_NUMBER}>
+            <img src="/moneypayment.gif" width="150" />
+          </a>
+          <span className={styles.divider} />
+          <div className={styles.flex} onClick={onLogout}>
+            <img src="/bomb.gif" width="100" height="100" />
+            <p>logout</p>
+          </div>
+        </div>
       </div>
     </div>
   );
